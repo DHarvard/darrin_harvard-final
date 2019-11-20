49ef58718c6fe5b5903a031a256ba94687ac00f6
Darrin Harvard <dbh_1991@hotmail.com>
2019-11-19 23:35:27 -0700

    Commiting characters array

---
+++ src/components/smash.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-layout>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Smash',
+
+  data: () => ({
+    
+  }),
+};
+</script>
